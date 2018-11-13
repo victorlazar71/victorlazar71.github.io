a1127696ae58ce9bf8fcf61a4ae5e8f7b35621d6
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-11-13 18:13:56 +0200

    Create cam.js

---
+++ cam.js
@@ -0,0 +1,12 @@
+ducument.getElement.ById("id_logic_version").innerHTML="Logic version 2018.11.13.0
+fuction on_ok(e)
+{
+ducument .get
+}
+function on_error(e)
+{
+}
+
+function star()
+{
+navigator.mediaDevias.getuserMedia().then(on_ok).catch(n_error);
