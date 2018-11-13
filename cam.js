6960c69ed7755d1c8aa8d7deb742756a9892278b
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-11-13 19:27:12 +0200

    Update cam.js

--- cam.js
+++ cam.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML="Logic version 2018.11.13.3";
+document.getElementById("id_logic_version").innerHTML="Logic version 2018.11.13.4";
 document.getElementById("id_video").addEventListener("mousedown",on_touch);
 document.getElementById("id_video").addEventListener("touchstat",on_touch);
 
@@ -20,7 +20,7 @@ navigator.mediaDevices.getUserMedia(p).then(on_ok).catch(on_error);
 function on_touch(e)
 {
   var canvas = document.getElementById("id_canvas");
-  var text =document.getContext("2d");
+  var context =canvas.getContext("2d");
   var video=document.ElementById("id_video");
   context.drawImagige(video , 0, 0);
 }
