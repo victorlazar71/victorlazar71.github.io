b2d7017a83f1c7c893d47b2e70f5999c534fc48c
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-11-20 18:05:20 +0200

    Create desenare.js

---
+++ desenare.js
@@ -0,0 +1,12 @@
+document.getElementBydo("id_logic_version ") .innerHTML= "Logic version= 2018.11.20.0";
+var canvas =document.getElementBydo("id_canvas");
+canvas .addEventListener("touchstar",on_touch);
+function on_touch(e)
+
+{
+for (var i=0:i<e.changed touches.length;i++);
+var canvas =canvas.get.Context(2d);
+contex.beginPath();
+   context.art( e.changed touches.item(i).pageX,e.changed touches.item(i).pageY ,20,0 2*Match.Pi);
+}
+context.stroke();
