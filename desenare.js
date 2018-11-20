3192e684a2390814aa9cec16fa5adf00a6e8a191
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-11-20 18:20:37 +0200

    Update desenare.js

--- desenare.js
+++ desenare.js
@@ -1,10 +1,10 @@
-document.getElementBydo("id_logic_version ") .innerHTML= "Logic version= 2018.11.20.1";
+document.getElementBydo("id_logic_version ") .innerHTML= "Logic version= 2018.11.20.2";
 var canvas =document.getElementBydo("id_canvas");
 canvas .addEventListener("touchstar",on_touch);
 function on_touch(e)
 
 {
-for (var i=0:i<e.changed touches.length:i++);
+for (var i=0:i<e.changed touches.length ;i++)
 var canvas =canvas.get.Context(2d);
 contex.beginPath();
    context.art( e.changed touches.item(i).pageX,e.changed touches.item(i).pageY ,20,0 2*Match.Pi);
