470942da21a800ae3c7b9b468f24ed3334add8fd
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-11-27 18:54:41 +0200

    Update desenare.js

--- desenare.js
+++ desenare.js
@@ -1,5 +1,5 @@
 document.getElementById("id_logic_version").innerHTML = 
-		"Logic version = 2018.11.27.5";
+		"Logic version = 2018.11.27.2";
 		
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
@@ -10,6 +10,10 @@ var rect = canvas.getBoundingClientRect();
 
 //var lastX = 0;
 //var lastY = 0;
+
+var last_position = {x: 0, y: 0, id: 0};
+
+var last_position_array = [];
 
 //---------------------------------
 function on_touch(e)
@@ -26,12 +30,11 @@ function on_touch(e)
 					0, 2 * Math.PI
 					);
 		context.stroke();
-		last_position.X = e.changedTouches.item(i).pageX;		
-		last_position.Y = e.changedTouches.item(i).pageY;
-		last_position.id=e.changedTouches.item(i).identifier;
-	last_position_array.push(last_position);
+		last_position.x = e.changedTouches.item(i).pageX;		
+		last_position.y = e.changedTouches.item(i).pageY;
+		last_position.id = e.changedTouches.item(i).identifier;
+		last_position_array.push(last_position);
 	}
-	
 }
 //---------------------------------
 function on_touch_move(e)
@@ -47,22 +50,22 @@ function on_touch_move(e)
 					10,
 					0, 2 * Math.PI
 					);
-		context.stroke();
-		var last_array=[];
-			var last_position={x:0,y:0, id:0};		
+		context.stroke();			
+					
 		context.beginPath();
-		context.lineWidth = 20;	
-		varr j=0;
-		for (var =0:j <  last_position_array.length; j++)
-		     if ( last_position_array[j].id = e.changedTouches.item(j).ident(fier)
-			 break;
-		context.moveTo(last_position_array[j].X - rect.left, last_position_array[j].Y - rect.top);
+		context.lineWidth = 20;		
+		var j = 0;
+		
+		for (; j < last_position_array.length; j++)
+			if (last_position_array[j].id == e.changedTouches.item(i).identifier)
+				break;
+			
+		context.moveTo(last_position_array[j].x - rect.left, last_position_array[j].y - rect.top);
 		context.lineTo(e.changedTouches.item(i).pageX - rect.left, 
 						e.changedTouches.item(i).pageY - rect.top);
 
-		last_position_array[j].X = e.changedTouches.item(i).pageX;		
-		last_position_array[j].Y = e.changedTouches.item(i).pageY;		
+		last_position_array[j].x = e.changedTouches.item(i).pageX;		
+		last_position_array[j].y = e.changedTouches.item(i).pageY;		
 		context.stroke();			
 	}	
 }
-
