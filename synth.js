22a202e6c35d13618c1efe415471d0b8dfcb954b
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-12-04 18:06:05 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version 2018.12.04.3"
+document.getElementById("id_logic_version").innerHTML = "Logic version 2018.12.04.4"
  var synth = window.speechSynthesis;
 
 function afiseaza_voci()
@@ -9,5 +9,10 @@ function afiseaza_voci()
   
   for (var i = 0; i <lista_voci.length; i++)
      document.getElementById("id_voci").innerHTML += lista_voci[i].lang +" " +lista_voci[i].name + "<br>" ;
-  
 }
+ function  vorbeste()
+{
+ var enunt =new speechSynthesisUtterance();
+ enunt.text= document.getElementById ("id_tex").value;
+ enunt.lang"en-US;"
+ synth.speak(enunt);
