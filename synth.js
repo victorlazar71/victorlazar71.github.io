cd038dfd8e136ab3c360070d94c72eed8a34d7cb
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-12-04 18:21:52 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,20 +1,19 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version 2018.12.04.6";
- var synth = window.speechSynthesis;
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.6";
+var synth = window.speechSynthesis;
 
 function afiseaza_voci()
 {
-  
-  
- var lista_voci = synth.getVoices();
-  
-  for (var i = 0; i <lista_voci.length; i++)
-     document.getElementById("id_voci").innerHTML += lista_voci[i].lang +" " +lista_voci[i].name + "<br>" ;
+	var lista_voci = synth.getVoices();
+	
+	for (var i = 0; i < lista_voci.length; i++)
+		document.getElementById("id_voci").innerHTML +=
+				lista_voci[i].lang + " " + lista_voci[i].name + "<br>";
 }
- function  vorbeste()
-{
- var enunt =new SpeechSynthesisUtterance();
- enunt.text= document.getElementById ("id_tex").value;
- enunt.lang"en-US;"
- synth.speak(enunt);
 
+function vorbeste()
+{
+	var enunt = new SpeechSynthesisUtterance();
+	enunt.text = document.getElementById("id_text").value;
+	enunt.lang = "en-US";
+	synth.speak(enunt);
 }
