b81d85c1948b368dbb47df75bea8ee0330da3293
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-12-04 17:45:42 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version 2018.12.04.1"
+document.getElementById("id_logic_version").innerHTML = "Logic version 2018.12.04.2"
 
 function afiseaza_voci()
 {
