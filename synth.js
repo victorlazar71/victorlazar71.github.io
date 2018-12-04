3a656403f0b645e583efd8c9e7a8066ec6da232e
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-12-04 18:48:25 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.6";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.7";
 var synth = window.speechSynthesis;
 
 function afiseaza_voci()
@@ -14,10 +14,15 @@ function vorbeste()
 {
 	var enunt = new SpeechSynthesisUtterance();
 	enunt.text = document.getElementById("id_text").value;
-
 	enunt.lang = "en-US";
 	enunt.onend=sfasit_vorveste;
+	document.getElementById("id_button_vorbeste").disabled=true;
+	
 	synth.speak(enunt);
 
 }
+function sfasit_vorveste()
+{
 
+document.getElementById("id_button_vorbeste").disabled=fals;
+}
