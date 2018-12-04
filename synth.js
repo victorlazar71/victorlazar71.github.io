b656d6af3487a05ec633b8d893526a64ab712594
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2018-12-04 19:05:57 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -15,7 +15,7 @@ function vorbeste()
 	var enunt = new SpeechSynthesisUtterance();
 	enunt.text = document.getElementById("id_text").value;
 	enunt.lang = "en-US";
-	enunt.onend=sfasit_vorbeste;
+	enunt.onend = sfasit_vorbeste;
 	document.getElementById("id_button_vorbeste").disabled=true;
 	synth.speak(enunt);
 
