3b32797e0570bf655091e6d53adaa82353ee75dc
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-01-15 18:30:33 +0200

    Create ec2.js

---
+++ ec2.js
@@ -0,0 +1,20 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2019.01.15.0";
+function rezolva()
+{
+
+
+var a= document.getElementBuId("id_a")value;
+var a= document.getElementBuId("id_b")value;
+var a= document.getElementBuId("id_c")value;
+var delta=b*b-4*a*c;
+varx1,x2;
+if(delta>=o){
+x1 = (-b + Math.sqrt(delta)) / (2 * a * c); 
+x1 = (-b - Math.sqrt(delta)) / (2 * a * c);
+}
+else{
+
+
+document,gitElementByld("id_x2").value=x1;
+ocument,gitElementByld("id_x2").value=x1;
+}
