cd9c8276d9c2d801ae9fbcd0d75a93b1accd3354
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-01-31 15:49:33 +0200

    Update gps.js

--- gps.js
+++ gps.js
@@ -1,15 +1,21 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2019.01.22.2";
-function onSuccess(heading)
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2019.01.22.3";
+
+
+function get_position()
 {
-    var element = document.getElementById('heading');
-    myHeading = (heading.magneticHeading).toFixed(2);
-    console.log("My Heading = " + myHeading);
-}
-function onError(compassError) {
-    alert('Compass error: ' + compassError.code);
-}
-var options = {
-    frequency: 500
+	navigator.geolocation.getCurrentPosition(on_gps_ok, on_gps_error);
 }
 
-watchID = navigator.compass.watchHeading(onSuccess, onError, options);
+function on_gps_ok(e)
+{
+	document.getElementById("id_lat").innerHTML = e.coords.latitude;
+	document.getElementById("id_long").innerHTML = e.coords.longitude;
+	document.getElementById("id_acc").innerHTML = e.coords.accuracy;
+	document.getElementById("id_alt").innerHTML = e.coords.altitude;
+	document.getElementById("id_acc_alt").innerHTML = e.coords.altitudeAccuracy;
+}
+
+function on_gps_error(e)
+{
+	alert("Eroare GPS");
+}
