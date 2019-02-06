86867d6d2c6c1bd38b3d870e539d69fcf1f42e76
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-02-06 10:27:52 +0200

    Update tema.js

--- tema.js
+++ tema.js
@@ -1,33 +1,30 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2019.02.06.2";
-document.getElementById("id_start").addEventListener("click", get_position);
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.14";
 
-//---------------------------
-function get_position()
+navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
+
+//---------------------------------
+function on_position_success(e)
 {
-	navigator.geolocation.getCurrentPosition(on_gps_ok, on_error);
-}
-//---------------------------
-function on_gps_ok(e)
-{
-	
 	 document.getElementById("id_lat").innerHTML ="Latitude="+ e.coords.latitude;
 	 document.getElementById("id_long").innerHTML ="Longitude="+ e.coords.longitude;
 	 document.getElementById("id_acc").innerHTML ="Accuracy="+ e.coords.accuracy + "m";
 	 document.getElementById("id_alt").innerHTML ="Altitude="+ e.coords.altitude + "m";
-         var map_str="https://maps.googleapis.com/maps/api/staticmap?"+
+	 
+	 var map_str="https://maps.googleapis.com/maps/api/staticmap?"+
 			"center="+ e.coords.latitude + "," + e.coords.longitude +
 			"&zoom=15" +
 			"&size=400x300" +
 			"&key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
 			"&markers=color:blue|label:A|" + e.coords.latitude + "," + e.coords.longitude +
-			"&path=color:red|" + e.coords.latitude + "," + e.coords.longitude +;
+			"&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|Sala Unirii Alba Iulia"
+			;
 			
 	 document.getElementById("id_img").src=map_str;
 }
-
-//---------------------------
-function on_error(e)
+//-------------------------------
+function on_position_failure(e)
 {
-	alert("Error" + e.error);
+	
+	alert("M-sm pirtdut!");
+	
 }
-//---------------------------
