8b22f05eb60a2c8c34114cc0b0a5930e4a7eaa34
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-02-06 18:17:08 +0200

    Update tema.js

--- tema.js
+++ tema.js
@@ -10,15 +10,24 @@ function on_position_success(e)
 	 document.getElementById("id_acc").innerHTML ="Accuracy="+ e.coords.accuracy + "m";
 	 document.getElementById("id_alt").innerHTML ="Altitude="+ e.coords.altitude + "m";
 	 
-	var map_str="https://maps.googleapis.com/maps/api/staticmap?"+
-			"center="+ e.coords.latitude + "," + e.coords.longitude +
-			"&zoom=15" +
-			"&size=400x300" +
-			"&key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
-			"&markers=color:blue|label:A|" + e.coords.latitude + "," + e.coords.longitude +
-			"&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|Căminul Studenţesc Nr. 1";
+	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+	
+	//var map_str="https://maps.googleapis.com/maps/api/staticmap?"+
+		//	"center="+ e.coords.latitude + "," + e.coords.longitude +
+		//	"&zoom=15" +
+		//	"&size=400x300" +
+		//	"&key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
+		//	"&markers=color:blue|label:A|" + e.coords.latitude + "," + e.coords.longitude +
+		//	"&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|Căminul Studenţesc Nr. 1";
 			
-	 document.getElementById("id_img").src=map_str;
+	// document.getElementById("id_img").src=map_str;
 }
 
 function on_position_failure(e)
