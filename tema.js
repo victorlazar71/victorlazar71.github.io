216c0ed196f67f5f728f68be9d616f03586f646d
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-02-06 11:05:40 +0200

    Update tema.js

--- tema.js
+++ tema.js
@@ -1,8 +1,8 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2019.02.08.8";
+document.getElementById("id_business_version").innerHTML = "Business version = 2019.02.08.9";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
-//---------------------------------
+
 function on_position_success(e)
 {
 	 document.getElementById("id_lat").innerHTML ="Latitude="+ e.coords.latitude;
@@ -10,18 +10,17 @@ function on_position_success(e)
 	 document.getElementById("id_acc").innerHTML ="Accuracy="+ e.coords.accuracy + "m";
 	 document.getElementById("id_alt").innerHTML ="Altitude="+ e.coords.altitude + "m";
 	 
-	 var map_str="https://maps.googleapis.com/maps/api/staticmap?"+
+	 var map_str="https://www.google.com/maps"+
 			"center="+ e.coords.latitude + "," + e.coords.longitude +
 			"&zoom=15" +
 			"&size=400x300" +
 			"&key=AIzaSyBflsfrBYWXqLzpqKW1HGF7npiwqfVrCEM"+
 			"&markers=color:blue|label:A|" + e.coords.latitude + "," + e.coords.longitude +
-			"&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|Sala Unirii Alba Iulia"
-			;
+			"&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|Căminul Studenţesc Nr. 1";
 			
 	 document.getElementById("id_img").src=map_str;
 }
-//-------------------------------
+
 function on_position_failure(e)
 {
 	
