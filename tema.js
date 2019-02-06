d4de19c7082d83df0c81b94cdc0b82c192fb61b4
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-02-06 13:27:36 +0200

    Update tema.js

--- tema.js
+++ tema.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2019.02.08.9";
+document.getElementById("id_business_version").innerHTML = "Business version = 2019.02.08.10";
 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
@@ -10,7 +10,7 @@ function on_position_success(e)
 	 document.getElementById("id_acc").innerHTML ="Accuracy="+ e.coords.accuracy + "m";
 	 document.getElementById("id_alt").innerHTML ="Altitude="+ e.coords.altitude + "m";
 	 
-	 var map_str="https://www.google.com/maps"+
+	 var map_str=""https://maps.googleapis.com/maps/api/js">"+
 			"center="+ e.coords.latitude + "," + e.coords.longitude +
 			"&zoom=15" +
 			"&size=400x300" +
