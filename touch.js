8883a081ff70631d209b1011ffca1ca4237f6065
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-02-07 15:06:37 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.08.1";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.07.2";
 var svg = document.getElementById("id_svg");
 svg.addEventListener("touchstart", on_touch_svg);
 svg.addEventListener("mousedown", on_touch_svg);
@@ -9,11 +9,11 @@ function on_touch_svg(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++)
 	{
-		var cerc = document.createElementNS("http://www.w3.org/2000/svg", "circle"); //creare element cerc in cazul nostru
-		cerc.setAttribute("cx", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
-		cerc.setAttribute("cy", e.changedTouches[i].pageY - svg_rect.top);
-		cerc.setAttribute("r", 20);
-		svg.appendChild(cerc);  // adaugare in svg ca si "copil" (subelement)
+		var patrat = document.createElementNS("http://www.w3.org/2000/svg", "square"); //creare element cerc in cazul nostru
+		patrat.setAttribute("cx", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
+		patrat.setAttribute("cy", e.changedTouches[i].pageY - svg_rect.top);
+		 patrat.setAttribute("r", 20);
+		svg.appendChild(patrat);  // adaugare in svg ca si "copil" (subelement)
 
 }
 }
