63b82248edad8e02be7f757c9ba61e2b1e8eacfd
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-02-07 15:22:45 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -9,7 +9,7 @@ function on_touch_svg(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++)
 	{
-		var patrat = document.createElementNS("http://www.w3.org/2000/svg", "square"); //creare element cerc in cazul nostru
+		var patrat = document.createElementNS("http://www.w3.org/2000/svg", "square"); //creare element patratin cazul nostru
 		patrat.setAttribute("cx", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
 		patrat.setAttribute("cy", e.changedTouches[i].pageY - svg_rect.top);
 		 patrat.setAttribute("r.",20);
