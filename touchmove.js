8801092f3cd2b58ba97b13649a366bba2b2a803c
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-02-07 14:36:15 +0200

    Update touchmove.js

--- touchmove.js
+++ touchmove.js
@@ -1,19 +1,12 @@
-document.getElementById("id_logic_version").innnerHTML="Logic:2029.02.07.0";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.08.2";
 var svg = document.getElementById("id_svg");
-var cerc1= document.getElementById("id_circle1");
-var cerc2= document.getElementById("id_circle2");
-cerc1.addEvenitListener("touchmove",On_touch_move1);
-cerc2.addEvenitListener("touchmove",On_touch_move2);
-function on_touch_move1(e)
+var cerc = document.getElementById("id_circle");
+
+cerc.addEventListener("touchmove", on_touch_move);
+var svg_rect = svg.getBoundingClientRect();
+
+function on_touch_move(e)
 {
-e.preventDefault();
-cerc1.setAttribute("cx",e.changedTouches[0].pageX);
-cerc1.setAttribute("cy",e.changedTouches[0].pageY-svd_rect.top);
-  }
-  function on_touch_move2(e)
-{
-e.preventDefault();
-cerc2.setAttribute("cx",e.changedTouches[0].pageX);
-cerc2.setAttribute("cy",e.changedTouches[0].pageY-svd_rect.top);
-  }
-  
+	e.preventDefault();
+	cerc.setAttribute("cx", e.changedTouches[0].pageX);
+	cerc.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
