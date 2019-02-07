7be08e88747d8e6c55a6f9cae16436ff482c7d28
victorlazar71 <44614009+victorlazar71@users.noreply.github.com>
2019-02-07 16:12:36 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.07.7";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.07.8";
 var svg = document.getElementById("id_svg");
 svg.addEventListener("touchstart", on_touch_svg);
 svg.addEventListener("mousedown", on_touch_svg);
@@ -9,13 +9,14 @@ function on_touch_svg(e)
 {
 	for (var i = 0; i < e.changedTouches.length; i++)
 	{
-		var patrat = document.createElementNS("http://www.w3.org/2000/svg", "square"); //creare element patratin cazul nostru
-		patrat.setAttribute("cx", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
-		patrat.setAttribute("cy", e.changedTouches[i].pageY - svg_rect.top);
-		patrat.setAttribute("width", 50);
-		patrat.setAttribute("height", 50);
-		svg.appendChild(patrat);  // adaugare in svg ca si "copil" (subelement)
-		patrat.clearRect(0, 0, 50, 50);
+		var cub = document.createElementNS("http://www.w3.org/2000/svg", "rect");	
+		cub.setAttribute("x", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
+		cub.setAttribute("y", e.changedTouches[i].pageY - svg_rect.top);
+		cub.setAttribute("width", 50);
+		cub.setAttribute("height", 50);
+		cub.setAttribute("fill", color);
+		svg.appendChild(cub)// adaugare in svg ca si "copil" (subelement)
+		cub.clearRect(0, 0, 50, 50);
 		var loopTimer = setTimeout("on_touch_svg("+e.changedTouches[i].pageX+", "+e.changedTouches[i].pageY - svg_rect.top+")", 200);
 		
 
